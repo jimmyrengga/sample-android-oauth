2dc5e2af560737ced2e11b38aefbc238e3c301e0
Jimmy Rengga <j.rengga@gmail.com>
2013-10-08 14:29:32 +0700

    1. tambah spinner account yang menampilkan data 'accounts' di dalam settings > accounts 2. tambah depedencies com.google.android.gms 3. tambah deskripsi readme

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-Sample Project Android Oauth Google
+# Contoh Aplikasi android dengan oauth google
 
+Deskripsi
+---------------
+Contoh pemakaian oauth google melalui android menggunakan maven. `{ <sdk>/extras/google/google_play_services/samples/auth }` 
 
+Pastikan Maven telah terinstall terlebih dahulu. Dikarenakan projek ini required terhadap libs `google-play-services_lib`, silahkan baca [gms-mvn-install][1] untuk mendapatkan lib tersebut ke dalam repository lokal maven.
+
+Thanks to :
+[https://github.com/JakeWharton]
+
+[1]: https://github.com/JakeWharton/gms-mvn-install/blob/master/README.md
--- src/com/github/jimmyrengga/sample/android/MainActivity.java
+++ src/com/github/jimmyrengga/sample/android/MainActivity.java
@@ -1,15 +1,29 @@
 package com.github.jimmyrengga.sample.android;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.google.android.gms.auth.GoogleAuthUtil;
 
 public class MainActivity extends Activity {
+
+    private String namesArray[];
+    private AccountManager mAccountManager;
+    private Spinner contactsSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        namesArray = getAccountNames();
+        contactsSpinner = initializeSpinner(
+                R.id.accounts_spinner, namesArray);
     }
 
     @Override
@@ -17,6 +31,25 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    private String[] getAccountNames() {
+        mAccountManager = AccountManager.get(this);
+        Account[] accounts = mAccountManager.getAccountsByType(GoogleAuthUtil.GOOGLE_ACCOUNT_TYPE);
+        String[] names = new String[accounts.length];
+        for (int i = 0; i < names.length; i++) {
+            names[i] = accounts[i].name;
+        }
+        return names;
+    }
+
+    private Spinner initializeSpinner(int id, String[] values) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_spinner_item, values);
+        Spinner spinner = (Spinner) findViewById(id);
+        spinner.setAdapter(adapter);
+
+        return spinner;
     }
     
 }
